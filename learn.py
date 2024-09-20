247d0913ca1f02435211a0de848a8dce02295e75
dasrohan <rohan.das@nucash.money>
2024-09-20 12:38:50 +0530

    py calc

--- learn.py
+++ learn.py
@@ -1,3 +1,31 @@
-c = 10+20
+def add(x, y):
+    return x + y
 
-print(c)
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Division by zero!"
+    else:
+        return x / y
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    result = add(num1, num2)
+elif op == "-":
+    result = subtract(num1, num2)
+elif op == "*":
+    result = multiply(num1, num2)
+elif op == "/":
+    result = divide(num1, num2)
+else:
+    result = "Invalid operator"
+
+print(result)
